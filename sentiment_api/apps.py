7fdb783d08aa1e3e284852fb7ba9008149179265
khalid abdulkadir <hanyak65@gmail.com>
2025-10-18 02:14:24 +0300

    fixed NLTK issues

--- sentiment_api/apps.py
+++ sentiment_api/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 import os
 import sys
 import joblib
@@ -15,29 +14,30 @@ class SentimentApiConfig(AppConfig):
     model = None  # Initialize a placeholder for the model
 
     def ready(self):
-        # This method is called once when the app is ready.
-
-        # Download NLTK data required for the TextCleaner
         try:
             nltk.data.find('tokenizers/punkt')
         except LookupError:
             nltk.download('punkt')
+
+        try:
+            nltk.data.find('tokenizers/punkt_tab')
+        except LookupError:
+            nltk.download('punkt_tab')
+
         try:
             nltk.data.find('corpora/stopwords')
         except LookupError:
             nltk.download('stopwords')
+
         try:
             nltk.data.find('corpora/wordnet')
         except LookupError:
             nltk.download('wordnet')
 
-        # Path to the trained model, relative to this app's directory
+        # Load the model as before
         app_path = os.path.dirname(__file__)
         MODEL_PATH = os.path.join(app_path, 'sentiment_analysis.pkl')
 
-        # This is the 'monkey patch'. It manually injects the TextCleaner class
-        # into the __main__ module, which is where pickle is looking for it.
         sys.modules['__main__'].TextCleaner = text_cleaner.TextCleaner
-
-        # Load the model and store it on the AppConfig instance
         self.model = joblib.load(MODEL_PATH)
+
